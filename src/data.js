e33fd6c3f4af4c7e1e4eef6e425c2ed178ffb25a
krsnamara <martinj.fitzpatrick@gmail.com>
2023-03-20 21:53:25 -0400

    building out componets, pages, assets, and seed data

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "http://www.picturecorrect.com/wp-content/uploads/2012/10/colorful-fashion-shoot-1.jpg",
+        title: "SUMMER SALE",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://www.picturecorrect.com/wp-content/uploads/2012/10/colorful-fashion-shoot-0.jpg",
+        title: "AUTUMN COLLECTION",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 3,
+        img: "https://hensel.eu/wp-content/uploads/2017/02/10scheme_04-480x686.jpg",
+        title: "WINTER SALE",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://cdn.shopify.com/s/files/1/0055/1242/products/EXP60PAZ-Aster-Blue-1_2048x.jpg?v=1520642568",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://pyxis.nymag.com/v1/imgs/1c6/841/5c709b37d80b170dab635914cdbdc9d683.rsquare.w600.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://cdn.shopify.com/s/files/1/0055/1242/files/Lifestyle-1-EXP52BMR_5f109608-4bd7-4d27-82ee-fd1d40428f02.jpg?844168297892011037",
+        title: "LIGHT JACKETS",
+    },
+]
+
+export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://www.prada.com/content/dam/pradabkg_products/S/SC5/SC551/1Y5SF0040/SC551_1Y5S_F0040_S_212_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.400.500.webp",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.prada.com/content/dam/pradabkg_products/U/UCS/UCS427/1XV2F0028/UCS427_1XV2_F0028_S_231_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.400.500.webp",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.prada.com/content/dam/pradabkg_products/U/UJN/UJN809/103JF0UB0/UJN809_103J_F0UB0_S_221_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.400.500.webp",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+]
