86f8c59d2c4a93ae4ba181a7cc19e29cbf6c776e
krsnamara <martinj.fitzpatrick@gmail.com>
2023-04-24 21:48:35 -0400

    items can now be added to cart and displayed dynamicaly on cart page. WIP to connect all the ui links

--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -8,7 +8,7 @@ const cartSlice = createSlice({
         total: 0,
     },
     reducers:{
-        addProduct:(state,action)=>{
+        addProduct: (state, action)=>{
             state.quantity += 1;
             state.products.push(action.payload);
             state.total += action.payload.price *action.payload.quantity;
