2dfb13dd66d433f213b227ffef16fc2dc6d0d89e
krsnamara <martinj.fitzpatrick@gmail.com>
2023-04-25 10:18:59 -0400

    scrollToTop feature added to all routes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <Provider store={store}>
-      <App />
-    </Provider> 
+      <ScrollToTop />
+      <Provider store={store}>
+        <App />
+      </Provider> 
     </Router> 
   </React.StrictMode>,
 );
